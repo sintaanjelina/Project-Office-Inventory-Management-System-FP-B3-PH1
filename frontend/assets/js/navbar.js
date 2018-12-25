1bbd45f2114eb4a831a0900cfaac09ffdc2dd33e
sintaanjelina <sinta.anjelina3930@gmail.com>
2018-12-26 05:55:46 +0700

    fix something for table

--- frontend/assets/js/navbar.js
+++ frontend/assets/js/navbar.js
@@ -1,9 +1,8 @@
-<script>
 	
-		$( '#sidebar .navbar-nav a' ).on( 'click', function () {
-			$( '#sidebar .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
-			$( this ).parent( 'li' ).addClass( 'active' );
-		});
+	$( '#sidebar .navbar-nav a' ).on( 'click', function () {
+		$( '#sidebar .navbar-nav' ).find( 'li.active' ).removeClass( 'active' );
+		$( this ).parent( 'li' ).addClass( 'active' );
+	});
 
 	$(function(){
     //ambil value action dari .handover-action
@@ -13,4 +12,16 @@
 	        $("input[name='handed_process_act']").val(value);
 	    });
 	});	
-</script>
+
+	$(document).ready(function(){
+	    $('#tbl_item').DataTable();
+	    $('#tbl_employee').DataTable();
+	    $('#tbl_requestact_history').DataTable();
+	    $('#tbl_approval_history').DataTable();	    
+	    $('#tbl_returned_item').DataTable();
+	    $('#tbl_onprocess_request').DataTable();
+	    $('#tbl_approval_req_list').DataTable();
+	    $('#tbl_my_request').DataTable();
+	    
+	} );
+
